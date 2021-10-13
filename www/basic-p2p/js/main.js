bade5ae958c2df520b4b443c97715823623407d0
Rida Tariq <rtariq@hawk.iit.edu>
2021-10-13 04:30:02 -0700

    Set up fresh ExpressJS install
    $npm install express --save

--- www/basic-p2p/js/main.js
+++ www/basic-p2p/js/main.js
@@ -1,92 +1,43 @@
-/***
- * Excerpted from "Programming WebRTC",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/ksrtc for more book information.
-***/
 'use strict';
 
-/**
- *  Global Variables: Configuration, $peer, and $self
- */
-
-
 
 /**
- *  Signaling-Channel Setup
- */
+* User-Interface Setup
+*/
+const namespace = prepareNamespace(window.location.hash, true);
+document.querySelector('#header h1')
+.innerText = 'Welcome to Room #' + namespace;
 
-
-
+document.querySelector('#call-button')
+.addEventListener('click', handleCallButton);
 /**
- * =========================================================================
- *  Begin Application-Specific Code
- * =========================================================================
- */
-
-
-
-/**
- *  User-Interface Setup
- */
-
- document.querySelector('#call-button')
- .addEventListener('click', handleCallButton);
-
-/**
- *  User-Media Setup
- */
-
+* User-Interface Functions and Callbacks
+*/
+function handleCallButton(event) {
+const callButton = event.target;
+if (callButton.className === 'join') {
+console.log('Joining the call...');
+callButton.className = 'leave';
+callButton.innerText = 'Leave Call';
+} else {
+console.log('Leaving the call...');
+callButton.className = 'join';
+callButton.innerText = 'Join Call';
+}
+}
 
 
 /**
- *  User-Interface Functions and Callbacks
- */
-
- function handleCallButton() {
- console.log('Call button clicked! Named callback function active!');
- }
-
-/**
- *  User-Media Functions
- */
-
-
-
-/**
- *  Call Features & Reset Functions
- */
-
-
-
-/**
- *  WebRTC Functions and Callbacks
- */
-
-
-
-/**
- * =========================================================================
- *  End Application-Specific Code
- * =========================================================================
- */
-
-
-
-/**
- *  Reusable WebRTC Functions and Callbacks
- */
-
-
-
-/**
- *  Signaling-Channel Functions and Callbacks
- */
-
-
-
-/**
- *  Utility Functions
- */
+* Utility Functions
+*/
+function prepareNamespace(hash, set_location) {
+let ns = hash.replace(/^#/, ''); // remove # from the hash
+if (/^[0-9]{7}$/.test(ns)) {
+console.log('Checked existing namespace', ns);
+return ns;
+}
+ns = Math.random().toString().substring(2, 9);
+console.log('Created new namespace', ns);
+if (set_location) window.location.hash = ns;
+return ns;
+}
