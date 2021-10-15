f9e0d230ae97110cd4b39118ee334640dc81c18c
Rida Tariq <rtariq@hawk.iit.edu>
2021-10-15 05:35:56 -0700

    Added comments in main.js file to improve readability.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,22 @@
 'use strict'
 
+//variable '$self' is used for things on users side of call
+//initially starts by setting audio to off and video to on
 const $self = {
   rtcConfig: null,
   constraints: {audio:false, video: true }
 };
 
+//peer object is used as person two in syscal
+//sets up a connection b/w two people ($self & $peer)
 const $peer = {
   connection: new RTCPeerConnection($self.rtcConfig)
 };
 
+/* set up a stream by grabbing self and peer*/
 requestUserMedia($self.constraints);
 
+//requests media usage from user in pop up
 async function requestUserMedia(constraints) {
   const video = document.querySelector('#self')
   $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -20,10 +26,16 @@ async function requestUserMedia(constraints) {
 /**
 * Socket Server Events and Callbacks
 */
+
+//namespace to use hash in window
 //const namespace = window.location.hash.substr(1);
+
 const button = document.querySelector('#call');
+
+//pass in namespace for particular hash
 const sc = io( { autoConnect: false} );
 //const sc = io(`/${namespace}`, { autoConnect: false} );
+
 button.addEventListener('click', function(){
   sc.open();
 });
