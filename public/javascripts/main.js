1315b0dcba58f851d4004aa9201aff405cc6068c
Rida Tariq <rtariq@hawk.iit.edu>
2021-11-20 04:53:21 -0700

    Put all material of script.js to main.js and remove script.js. Add jquery script API file in index.html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -192,3 +192,27 @@ console.log('Created new namespace', ns);
 if (set_location) window.location.hash = ns;
 return ns;
 }
+
+// feedback form
+
+$(document).ready(function() {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+  var submitId = document.getElementById("submitId");
+});
+
+submitId.addEventListener("click", function(){document.getElementById('form').addEventListener('submit', submitFunc);});
+
+function submitFunc() {
+  var inName = document.getElementById("nameInput").value;
+  var inEmail = document.getElementById("emailInput").value;
+  var inSubject = document.getElementById("subjectInput").value;
+
+
+  if ((inName == "") || (inEmail == "") || (inSubject == ""))
+    alert("Fill out all the required info.");
+
+  else
+    alert("Thank you, " + inName + "! We appreciate your time.");
+
+}
